069ab2e404a078661b67db05c7b7968433b667a2
Eldar Zhamilev <waxenov@mail.ru>
2024-03-02 08:46:16 +0600

    Добавлены страницы

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,8 +12,20 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-nav-link href="{{ route('dashboard') }}" wire:navigate :active="request()->routeIs('dashboard')">
                         {{ __('Главная') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('orders') }}" wire:navigate :active="request()->routeIs('orders')">
+                        {{ __('Заказы') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('carriers') }}" wire:navigate :active="request()->routeIs('carriers')">
+                        {{ __('Перевозчики') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('create') }}" wire:navigate :active="request()->routeIs('create')">
+                        {{ __('Создать') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('about') }}" wire:navigate :active="request()->routeIs('about')">
+                        {{ __('О нас') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -139,10 +151,23 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link href="{{ route('dashboard') }}" wire:navigate :active="request()->routeIs('dashboard')">
                 {{ __('Главная') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('orders') }}" wire:navigate :active="request()->routeIs('orders')">
+                {{ __('Заказы') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('carriers') }}" wire:navigate :active="request()->routeIs('carriers')">
+                {{ __('Перевозчики') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('create') }}" wire:navigate :active="request()->routeIs('create')">
+                {{ __('Создать') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('about') }}" wire:navigate :active="request()->routeIs('about')">
+                {{ __('О нас') }}
+            </x-responsive-nav-link>
         </div>
+        
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,20 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/orders', function () {
+        return view('orders');
+    })->name('orders');
+
+    Route::get('/carriers', function () {
+        return view('carriers');
+    })->name('carriers');
+
+    Route::get('/create', function () {
+        return view('create');
+    })->name('create');
+
+    Route::get('/about', function () {
+        return view('about');
+    })->name('about');
 });
