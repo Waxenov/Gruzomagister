80e6368f9c8b6cd0a8750d484460409b332f79f4
Eldar Zhamilev <waxenov@mail.ru>
2024-03-04 12:13:44 +0600

    Реализация  вывода данных груза

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -8,7 +8,24 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                
+                <div class="flex justify-center max-w-full mx-auto sm:px-6 lg:px-8">
+                    <table class="border-2">
+                        @foreach ($formData as $data)
+                        <tr class="border-2">
+                            <td> {{ $data->id }} </td>
+                            <td> {{ $data->created_at }} </td>
+                            <td> {{ $data->cargo_type }} </td>
+                            <td> {{ $data->weight }} </td>
+                            <td> {{ $data->ready_date }} </td>
+                            <td> {{ $data->load_place }} </td>
+                            <td> {{ $data->unload_place }} </td>
+                            <td> {{ $data->truck_type }} </td>
+                            <td> {{ $data->surname }} {{ $data->name }} {{ $data->patronymic }} </td>
+                            <td> {{ $data->phone }} </td>
+                        </tr>
+                        @endforeach
+                    </table>
+                </div>
             </div>
         </div>
     </div>
