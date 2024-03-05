c79a5872806bdd59faf2bc761223aea3d8eaee85
Eldar Zhamilev <waxenov@mail.ru>
2024-03-05 12:06:32 +0600

    Создание дизайна страниц

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <!-- Styles -->
         @livewireStyles
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-[#EDE3E3]">
         <x-banner />
 
         <div class="bg-gray-100">
@@ -41,5 +41,66 @@
         @stack('modals')
 
         @livewireScripts
+        
+
+            <footer class="bg-white">
+                <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                    <div class="md:flex md:justify-between">
+                    <div class="mb-6 md:mb-0">
+                        <a href="http://gruzomagister/dashboard" class="flex items-center">
+                            <span class="self-center text-2xl font-semibold whitespace-nowrap">Грузомагистр</span>
+                        </a>
+                    </div>
+                    <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                        <div>
+                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Разработка</h2>
+                            <ul class="text-gray-500 font-medium">                                
+                                <li class="mb-4">
+                                    <a href="https://jetstream.laravel.com/introduction.html" class="hover:underline">Laravel Jetstream</a>
+                                </li>
+                                <li class="mb-4">
+                                    <a href="https://livewire.laravel.com/" class="hover:underline">Livewire</a>
+                                </li>
+                                <li>
+                                    <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                                </li>
+                                <li class="mb-4">
+                                    <a href="https://vitejs.dev/" class="hover:underline">Livewire</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Создатель</h2>
+                            <ul class="text-gray-500 font-medium">
+                                <li class="mb-4">
+                                    <a href="https://github.com/Waxenov/Gruzomagister" class="hover:underline ">Github</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Правила</h2>
+                            <ul class="text-gray-500 font-medium">
+                                <li class="mb-4">
+                                    <a href="#" class="hover:underline">Политика конфиденциальности</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="hover:underline">Правила использования</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+                <div class="sm:flex sm:items-center sm:justify-between">
+                    <span class="text-sm text-gray-500 sm:text-center">© 2024 <a href="http://gruzomagister/dashboard" class="hover:underline">Грузомагистр</a>. Выпускная квалификационная работа. Не является рабочим проектом.
+                    </span>
+                    <div class="flex mt-4 sm:justify-center sm:mt-0">
+
+                    </div>
+                </div>
+                </div>
+            </footer>
+
+
     </body>
 </html>
