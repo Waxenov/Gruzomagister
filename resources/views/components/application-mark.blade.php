a593d8365015153932bcddf763e99c0a895191c2
Eldar Zhamilev <waxenov@mail.ru>
2024-03-10 22:41:47 +0600

    Обновление логотипа

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,19 +1,72 @@
-<svg viewBox="0 0 402 56.19" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+<svg id="fon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 487.5 60" {{ $attributes }}>
     <defs>
-<style>.cls-1{fill:#ff0028;}.cls-2{fill:none;stroke:#000;stroke-miterlimit:10;stroke-width:5px;}</style>
-    </defs>
-        <path d="M30.59,52.09V14.31h27V19.7H36V52.09Z" transform="translate(-12 -14.31)" />
-        <path d="M63,57.49V25.1h5.4v5.4h5.39v5.4H68.38v5.39H84.57V30.5H73.77V25.1h10.8v5.4H90V41.29h-5.4v5.4H68.38v10.8Z" transform="translate(-12 -14.31)" />
-        <path d="M95.36,57.49v-5.4H117v-5.4H100.76v-5.4h-5.4V25.1h5.4V41.29H117V25.1h5.4v27H117v5.4Z" transform="translate(-12 -14.31)" />
-        <path d="M133.15,52.09v-5.4h-5.4v-5.4h5.4v5.4h16.19v-5.4h-10.8V35.9h10.8V30.5H133.15v5.4h-5.4V30.5h5.4V25.1h16.19v5.4h5.4v5.4h-5.4v5.39h5.4v5.4h-5.4v5.4Z" transform="translate(-12 -14.31)" />
-        <path d="M165.53,52.09v-5.4h16.19V30.5H165.53V46.69h-5.4V30.5h5.4V25.1h16.19v5.4h5.4V46.69h-5.4v5.4Z" transform="translate(-12 -14.31)" />
-        <path class="cls-1" d="M192.52,52.09V14.31h5.4V19.7h5.39v5.4h5.4V19.7h5.4V14.31h5.4V52.09h-5.4v-27h-5.4v5.4h-5.4V25.1h-5.39v27Z" transform="translate(-12 -14.31)" />
-        <path class="cls-1" d="M230.3,52.09v-5.4h16.19v-5.4H230.3v5.4h-5.4v-5.4h5.4V35.9h16.19V30.5H230.3V25.1h16.19v5.4h5.4V52.09Z" transform="translate(-12 -14.31)" />
-        <path class="cls-1" d="M262.69,52.09v-27h21.59v5.4h-16.2V52.09Z" transform="translate(-12 -14.31)" />
-        <path class="cls-1" d="M289.67,52.09v-27h5.4V41.29h5.4V35.9h5.4V30.5h5.39V25.1h5.4v27h-5.4V35.9h-5.39v5.39h-5.4v5.4h-5.4v5.4Z" transform="translate(-12 -14.31)" />
-        <path class="cls-1" d="M327.46,52.09v-5.4h-5.4V30.5h5.4V25.1h16.19v5.4h5.4v5.4h-5.4V30.5H327.46V46.69h16.19v-5.4h5.4v5.4h-5.4v5.4Z" transform="translate(-12 -14.31)" />
-        <path class="cls-1" d="M365.24,52.09V30.5h-10.8V25.1h27v5.4h-10.8V52.09Z" transform="translate(-12 -14.31)" />
-        <path class="cls-1" d="M386.83,57.49V25.1h5.39v5.4h5.4v5.4h-5.4v5.39h16.2V30.5h-10.8V25.1h10.8v5.4h5.4V41.29h-5.4v5.4h-16.2v10.8Z" transform="translate(-12 -14.31)" />
-        <polyline id="text" class="cls-2" points="2.5 0.19 2.5 53.19 402 53.69" />
-</svg>
+        <style>
+            .cls-1 {
+                fill: #ff0040;
+            }
 
+            .cls-2,
+            .cls-5 {
+                fill: none;
+            }
+
+            .cls-2,
+            .cls-4 {
+                stroke: #000;
+            }
+
+            .cls-2,
+            .cls-3,
+            .cls-4,
+            .cls-5,
+            .cls-6 {
+                stroke-miterlimit: 10;
+                stroke-width: 6px;
+            }
+
+            .cls-3,
+            .cls-4,
+            .cls-6 {
+                fill: #fff;
+            }
+
+            .cls-3 {
+                stroke: #000015;
+            }
+
+            .cls-3,
+            .cls-4,
+            .cls-5,
+            .cls-6 {
+                stroke-linecap: square;
+            }
+
+            .cls-5,
+            .cls-6 {
+                stroke: #ff0040;
+            }
+
+        </style>
+    </defs>
+    <path d="M106.59,47V4.31h27V9.7H112V47Z" transform="translate(-2.5 -1.5)" />
+    <path d="M144.38,31.29h16.19V20.5H144V15.1h16.57v5.4H166V31.29h-5.4v5.4H144.38v10.8H139V15.1h5.4" transform="translate(-2.5 -1.5)" />
+    <path d="M171.36,47.49v-5.4H193v-5.4H176.76v-5.4h-5.4V15.1h5.4V31.29H193V15.1h5.4v27H193v5.4Z" transform="translate(-2.5 -1.5)" />
+    <polygon points="206.65 40.59 206.65 35.19 201.25 35.19 201.25 29.8 206.65 29.8 206.65 35.19 222.84 35.19 222.84 29.8 212.04 29.8 212.04 24.4 222.84 24.4 222.84 19 206.65 19 206.65 24.4 201.25 24.4 201.25 19 206.65 19 206.65 13.6 222.84 13.6 222.84 19 228.24 19 228.24 24.4 222.84 24.4 222.84 29.8 228.24 29.8 228.24 35.19 222.84 35.19 222.84 40.59 206.65 40.59" />
+    <path d="M241.53,42.09v-5.4h16.19V20.5H241.53V36.69h-5.4V20.5h5.4V15.1h16.19v5.4h5.4V36.69h-5.4v5.4Z" transform="translate(-2.5 -1.5)" />
+    <path class="cls-1" d="M268.52,47.09V4.31h5.4V9.7h5.39v5.4h5.4V9.7h5.4V4.31h5.4V47.09h-5.4v-32h-5.4v5.4h-5.4V15.1h-5.39v32Z" transform="translate(-2.5 -1.5)" />
+    <path class="cls-1" d="M306.3,42.09v-5.4h16.19v-5.4H306.3v5.4h-5.4v-5.4h5.4V25.9h16.19V20.5H306.3V15.1h16.19v5.4h5.4V42.09Z" transform="translate(-2.5 -1.5)" />
+    <path class="cls-1" d="M338.69,42.09v-27h21.59v5.4h-16.2V42.09Z" transform="translate(-2.5 -1.5)" />
+    <path class="cls-1" d="M365.67,42.09v-27h5.4V31.29h5.4V25.9h5.4V20.5h5.39V15.1h5.4v27h-5.4V25.9h-5.39v5.39h-5.4v5.4h-5.4v5.4Z" transform="translate(-2.5 -1.5)" />
+    <path class="cls-1" d="M403.46,42.09v-5.4h-5.4V20.5h5.4V15.1h16.19v5.4h5.4v5.4h-5.4V20.5H403.46V36.69h16.19v-5.4h5.4v5.4h-5.4v5.4Z" transform="translate(-2.5 -1.5)" />
+    <path class="cls-1" d="M441.24,42.09V20.5h-10.8V15.1h27v5.4h-10.8V42.09Z" transform="translate(-2.5 -1.5)" />
+    <path class="cls-1" d="M473.62,20.5m-5.4,10.79h16.2V20.5H468V15.1h16.42v5.4h5.4V31.29h-5.4v5.4h-16.2v10.8h-5.39V15.1h5.39" transform="translate(-2.5 -1.5)" />
+    <polyline id="text" class="cls-2" points="88 3 88 56 487.5 56.5" />
+    <polyline class="cls-3" points="3 57 3 57 59 57 59 57" />
+    <path class="cls-1" d="M28.24,6.9V23.1H22.85V6.9h5.39V1.51h36.2V6.9" transform="translate(-2.5 -1.5)" />
+    <polyline class="cls-1" points="5.5 26.7 5.5 21.3 56 21.3 56 26.7" />
+    <polyline class="cls-1" points="0.03 54.2 0.03 26.5 5.5 26.5 5.5 54.2" />
+    <polyline class="cls-4" points="59 57 59 3 59 3" />
+    <line class="cls-5" x1="73.5" y1="11.5" x2="73.5" y2="51.5" />
+    <line class="cls-2" x1="128.07" y1="5.5" x2="128.07" y2="13.5" />
+    <polyline class="cls-6" points="53.5 51.2 53.5 3 53.5 3" />
+</svg>
