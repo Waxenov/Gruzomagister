f621e1bc00b10367485fa81cc67d97b5bfad1175
Eldar Zhamilev <waxenov@mail.ru>
2024-03-24 08:48:27 +0600

    Реализация админ панели и поиска по заказам

--- resources/views/carrier/orders.blade.php
+++ resources/views/carrier/orders.blade.php
@@ -1,10 +1,8 @@
 <x-app-layout>
     <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <button id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-        <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
-        <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-        </button>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            <!-- заголовок страницы с описанием -->
             <div class="flex flex-col md:flex-row justify-between items-start md:items-center">
                 <h2 class=" -mt-32 font-bold text-4xl text-gray-800 leading-tight">
                     {{ __('Вам заказы') }}
@@ -14,18 +12,31 @@
                         {{ __('Как это работает') }}
                     </h3>
                     <p class="font-normal text-xl text-[#876368] leading-tight mt-4">
-                    Грузы заказчиков теперь ваши заказы<br>
-                    Вы можете выбрать какой заказ вам удобно доставить<br>
-                    Когда выбор сделан — свяжитесь с заказчиком<br>
-                    И наконец уточните данные заказа<br>
+                        Грузы заказчиков теперь ваши заказы<br>
+                        Вы можете выбрать какой заказ вам удобно доставить<br>
+                        Когда выбор сделан — свяжитесь с заказчиком<br>
+                        И наконец уточните данные заказа<br>
                     </p>
                 </div>
             </div>
+
+            <!-- поиск -->
             <div class="flex mx-auto mb-8">
                 <div class="relative flex-1">
-                    <input type="text" id="simple-search" class="block w-96 px-4 py-2.5 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-[#FF0015] focus:border-[#FF0015] focus:outline-none" placeholder="Ищите и найдете">
+                    <form action="{{ route('orders') }}" method="GET">
+                        <button type="submit">
+                            <!-- иконка для кнопки поиска -->
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </button>
+                        <input type="text" name="search" class="block w-96 px-4 py-2.5 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-[#FF0015] focus:border-[#FF0015] focus:outline-none" placeholder="Ищите и найдете">
+                        <button type="submit">Показать все</button>
+                    </form>
                 </div>
             </div>
+
+            <!-- таблица заказов -->
             <div class="relative  overflow-x-auto ">
                 <table class="w-full  text-sm text-left rtl:text-right text-gray-500">
                     <thead class="text-xs  text-center text-gray-700 uppercase bg-[#ffffff]">
@@ -33,6 +44,14 @@
                             <th scope="col" class="px-6 py-3 rounded-tl-[15px]  bg-[#FF0015] text-white">
                                 Номер
                             </th>
+
+                            <!-- если пользователь админ - столбец удалить -->
+                            @if(Auth::user()->role === 'admin')
+                            <th scope="col" class="px-4 py-3 bg-black text-white">
+                                Удалить
+                            </th>
+                            @endif
+                            
                             <th scope="col" class="px-6 py-3 text-center">
                                 Заказчик
                             </th>
@@ -66,9 +85,26 @@
                         </tr>
                     </thead>
                     <tbody>
+
+                    <!-- цикл для вывода данных о заказах -->
                     @foreach($formData as $data)
                         <tr class="bg-[#ffffff] text-center border-b-2 border-b-[#EDE3E7]">
-                            <td>{{ $data->id }}</td>
+                            <td class="text-[#FF5267] font-black text-base">#{{ $data->id }}</td>
+
+                            <!-- если пользователь админ - кнопка удаления заказа -->
+                            @if(Auth::user()->role === 'admin')
+                            <td>
+                                <form action="{{ route('orders.destroy', $data->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="text-white bg-black font-black text-2xl px-5 py-3 text-center me-2 mb-2 mt-2">
+                                        ✕  
+                                    </button>
+                                </form>
+                            </td>
+                            @endif
+
+                            <!-- данные о заказе -->
                             <td>{{ $data->surname }}  {{ $data->name }}  {{ $data->patronymic }}</td>
                             <td>{{ $data->created_at }}</td>
                             <td>{{ $data->cargo_type }}</td>
@@ -83,7 +119,7 @@
                     @endforeach
                     </tbody>
                 </table>
-                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
--- resources/views/client/orders.blade.php
+++ resources/views/client/orders.blade.php
@@ -1,12 +1,10 @@
 <x-app-layout>
     <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <button id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-        <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
-        <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-        </button>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            <!-- заголовок страницы и описание -->
             <div class="flex flex-col md:flex-row justify-between items-start md:items-center">
-                <h2 class=" -mt-32 font-bold text-4xl text-gray-800 leading-tight">
+                <h2 class="-mt-32 font-bold text-4xl text-gray-800 leading-tight">
                     {{ __('Ваши заказы') }}
                 </h2>
                 <div class="md:ml-10">
@@ -14,23 +12,36 @@
                         {{ __('Как это работает') }}
                     </h3>
                     <p class="font-normal text-xl text-[#876368] leading-tight mt-4">
-                    Ваши грузы теперь наши заказы<br>
-                    Дождитесь отклика перевозчика<br>
-                    И наконец уточните данные заказа<br>
-                    Если не любите ждать — <a class="font-medium text-xl text-[#876368] leading-tight mt-4" href="{{ route('carriers') }}">Перевозчики</a>
+                        Ваши грузы теперь наши заказы<br>
+                        Дождитесь отклика перевозчика<br>
+                        И наконец уточните данные заказа<br>
+                        Если не любите ждать — <a class="font-medium text-xl text-[#876368] leading-tight mt-4" href="{{ route('carriers') }}">Перевозчики</a>
                     </p>
                 </div>
             </div>
+
+            <!-- поиск -->
             <div class="flex mx-auto mb-8">
                 <div class="relative flex-1">
-                    <input type="text" id="simple-search" class="block w-96 px-4 py-2.5 bg-slate-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-[#FF0015] focus:border-[#FF0015] focus:outline-none" placeholder="Ищите и найдете">
+                    <form action="{{ route('orders') }}" method="GET">
+                        <button type="submit">
+                            <!-- иконка для кнопки поиска -->
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </button>
+                        <input type="text" name="search" class="block w-96 px-4 py-2.5 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-[#FF0015] focus:border-[#FF0015] focus:outline-none" placeholder="Ищите и найдете">
+                        <button type="submit">Показать все</button>
+                    </form>
                 </div>
             </div>
-            <div class="relative  overflow-x-auto ">
-                <table class="w-full  text-sm text-left rtl:text-right text-gray-500">
+
+            <!-- таблица заказов -->
+            <div class="relative overflow-x-auto">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500">
                     <thead class="text-xs text-center text-gray-700 uppercase bg-[#ffffff]">
-                        <tr class=" max-w-8 text-center">
-                            <th scope="col" class="px-6 py-3 rounded-tl-[15px]  bg-[#FF0015] text-white">
+                        <tr class="max-w-8 text-center">
+                            <th scope="col" class="px-6 py-3 rounded-tl-[15px] bg-[#FF0015] text-white">
                                 Номер
                             </th>
                             <th scope="col" class="px-6 py-3">
@@ -63,7 +74,9 @@
                         </tr>
                     </thead>
                     <tbody>
-                    @foreach($userOrders as $order)
+
+                        <!-- вывод заказов заказчика -->
+                        @foreach($userOrders as $order)
                         <tr class="bg-[#ffffff] text-center py-6 border-b-2 border-b-[#EDE3E7]">
                             <td class="text-[#FF5267] font-black text-base">#{{ $order->id }}</td>
                             <td>{{ $order->created_at }}</td>
@@ -75,27 +88,31 @@
                             <td>{{ $order->unload_place }}</td>
                             <td>{{ $order->truck_type }}</td>
                             <td>
+
+                                <!-- кнопка удаления заказа -->
                                 <form action="{{ route('orders.destroy', $order->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
+                                    @csrf
+                                    @method('DELETE')
                                     <button type="submit" class="text-white bg-[#FF5267] font-black rounded-full text-base px-7 py-2.5 text-center me-2 mb-2 mt-2">
                                         X
                                     </button>
                                 </form>
                             </td>
                         </tr>
-                    @endforeach
-                    <tr class="bg-[#ffffff] text-center border-b-2 border-b-[#EDE3E7]">
-                            <td class="px-6 py-6 text-[#FF5267] rounded-bl-[15px]"> </td>
-                            <td class="px-6 py-6 text-[#606060]"> </td>
-                            <td class="px-6 py-6 text-[#606060]"> </td>
-                            <td class="px-6 py-6 text-[#606060]"> </td>
-                            <td class="px-6 py-6 text-[#606060]"> </td>
-                            <td class="px-6 py-6 text-[#606060]"> </td>
-                            <td class="px-6 py-6 text-[#606060]"> </td>
-                            <td class="px-6 py-6 text-[#606060]"> </td>
-                            <td class="px-6 py-6 text-[#606060]"> </td>
-                            <td class="px-6 py-6 text-[#FF5267] rounded-br-[15px]"> </td>
+                        @endforeach
+
+                        <!-- конечная строчка -->
+                        <tr class="bg-[#ffffff] text-center border-b-2 border-b-[#EDE3E7]">
+                            <td class="px-6 py-6 text-[#FF5267] rounded-bl-[15px] font-black text-xl"> ... </td>
+                            <td class="px-6 py-6 text-[#606060] font-black text-xl"> ... </td>
+                            <td class="px-6 py-6 text-[#606060] font-black text-xl"> ... </td>
+                            <td class="px-6 py-6 text-[#606060] font-black text-xl"> ... </td>
+                            <td class="px-6 py-6 text-[#606060] font-black text-xl"> ... </td>
+                            <td class="px-6 py-6 text-[#606060] font-black text-xl"> ... </td>
+                            <td class="px-6 py-6 text-[#606060] font-black text-xl"> ... </td>
+                            <td class="px-6 py-6 text-[#606060] font-black text-xl"> ... </td>
+                            <td class="px-6 py-6 text-[#606060] font-black text-xl"> ... </td>
+                            <td class="px-6 py-6 text-[#FF5267] rounded-br-[15px] font-black text-xl"> ... </td>
                         </tr>
                     </tbody>
                 </table>
