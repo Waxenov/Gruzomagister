1d583cba30d1b0884f8c02bf02b990fa279ad518
Eldar Zhamilev <waxenov@mail.ru>
2024-03-24 08:54:28 +0600

    Улучшение пользовательского интерфейса

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,155 @@
 <x-app-layout>
 
+    <!-- о компании -->
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-6xl mx-auto sm:px-8 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="flex justify-between items-start md:items-center">
-                    <h2 class=" mt-10 mx-10 font-bold text-4xl text-gray-800 leading-tight">
-                        {{ __('Мы двигаем мир') }}
-                    </h2>
-                </div>
-                <div class="md:ml-10 p-10">
-                    <p class="font-normal text-xl text-gray-600 leading-tight">
-                    Более трёх месяцев компания ГрузоМагистр является одним из лучших  поставщиков логистических услуг по Омской области. У нас есть опыт и экспертные знания, чтобы обеспечить безопасную и эффективную транспортировку ваших грузов. При небольшой сети партнеров, мы предлагаем широкий спектр услуг для наших клиентов.
-                    </p>
-                    <br>
-                    <button type="button" class="flex text-white bg-[#FF0015] hover:bg-[#FF5267] focus:ring-4 focus:outline-none font-medium rounded-lg text-xl w-full sm:w-auto px-24 py-2.5 text-center">@if(Auth::check())<a href="{{ route('create') }}">Начать</a>@else<a href="{{ route('register') }}">Начать</a>@endif</button>
-                </div>
+                <section class="max-h-[500px]  bg-bottom bg-no-repeat bg-[url('https://images.unsplash.com/photo-1558708102-48842c25dbce?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-blend-multiply">
+                    <div class="px-4 mx-auto max-w-screen-xl text-center lg:py-56">
+                        <h1 class="-my-20 mb-4 text-2xl font-bold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Мы двигаем мир</h1>
+                        <p class=" text-lg font-normal text-white lg:text-xl sm:px-16 lg:px-48">Более трёх месяцев компания ГрузоМагистр является одним из лучших поставщиков логистических услуг по Омской области. У нас есть опыт и экспертные знания, чтобы обеспечить безопасную и эффективную транспортировку ваших грузов. При небольшой сети партнеров, мы предлагаем широкий спектр услуг.</p>
+                    </div>
+                </section>
             </div>
         </div>
+    </div>
+
+    <!-- о нас -->
+    <div class="max-w-6xl mx-auto sm:px-8 lg:px-8">
+        <section class="bg-white dark:bg-[#191919] rounded-[15px] border-8 border-white  dark:border-[#191919]">
+            <h2 class="p-5 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Кто мы на самом деле</h2>
+            <h2 class="p-5 -my-5 text-2xl tracking-tight font-normal text-gray-900 dark:text-white">Ваши любимчики</h2>
+            <div class="px-4 mx-auto max-w-screen-xl lg:px-6">
+                <div class="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+                    <div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                портал ГрузоМагистр создан в Омске, Россия
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">30.01.2024</p>
+                        </div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                Пример
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">00.00.2024</p>
+                        </div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                Пример
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">00.00.2024</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <!-- как работает -->
+    <div class="py-12 max-w-6xl mx-auto sm:px-8 lg:px-8">
+        <section class="bg-white dark:bg-[#191919] rounded-[15px] border-8 border-white  dark:border-[#191919]">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+                <h2 class="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Как работает этот сайт</h2>
+                <div class="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+                    <div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+                                </svg>
+                                Что на странице "Заказы"?
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">Список заказов заказчика/перевозчика.</p>
+                        </div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+                                </svg>
+                                Заголовок?
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">Объяснение.</p>
+                        </div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+                                </svg>
+                                Заголовок?
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">Объяснение.</p>
+                        </div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+                                </svg>
+                                Заголовок?
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">Объяснение.</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+                                </svg>
+                                Заголовок?
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">Объяснение.</p>
+                        </div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+                                </svg>
+                                Заголовок?
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">Объяснение.</p>
+                        </div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+                                </svg>
+                                Заголовок?
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">Объяснение.</p>
+                        </div>
+                        <div class="mb-10">
+                            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                                <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+                                </svg>
+                                Заголовок?
+                            </h3>
+                            <p class="text-gray-500 dark:text-gray-400">Объяснение.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- о проекте -->
+            <div class="bg-[#f4f4f4] dark:bg-[#202020] py-24 sm:py-32 rounded-[15px]">
+                <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                    <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+                        <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                            <dt class="text-base leading-7 text-gray-600 dark:text-gray-400">Заняла разработка проекта</dt>
+                            <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 dark:text-white sm:text-5xl">105 часов</dd>
+                        </div>
+                        <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                            <dt class="text-base leading-7 text-gray-600 dark:text-gray-400">Пройдено в разработке проекта</dt>
+                            <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 dark:text-white sm:text-5xl">16 версий</dd>
+                        </div>
+                        <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+                            <dt class="text-base leading-7 text-gray-600 dark:text-gray-400">Потрачено на разработку</dt>
+                            <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 dark:text-white sm:text-5xl">999 нервов</dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+        </section>
     </div>
 </x-app-layout>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,22 +1,123 @@
 <x-app-layout>
-    <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class=" py-10">
-                <section class="bg-white dark:bg-gray-900 shadow-xl sm:rounded-lg">
-                    <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-                        <div class="font-normal text-gray-500 sm:text-lg dark:text-gray-400">
-                            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Не изобретаем велосипед, а улучшаем что есть</h2>
-                            <p class="mb-4">Мы стратеги, дизайнеры и разработчики. Новаторы и специалисты по решению проблем. Достаточно маленькие, чтобы быть простыми и быстрыми, но достаточно большие, чтобы обеспечить желаемый объем работ в нужном вам темпе. Достаточно маленький, чтобы быть простым и быстрым, но достаточно большой, чтобы обеспечить желаемый объем в нужном вам темпе.</p>
-                            <span class="inline-flex items-center rounded-md bg-red-50 px-2 p-2 text-sm font-medium text-[#FF0015] ring-1 ring-inset ring-red-600/10">Мы это то что вам нужно</span>
+
+    <div>
+        <div class="max-w-6xl mx-auto">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-[15px]">
+            <section class="max-h-[500px] bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1682940904270-d13b152a76bf?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-gray-200 bg-blend-multiply">
+                    <div class="px-4 mx-auto max-w-screen-xl text-center lg:py-56">
+                        
+                        <!-- заголовок -->
+                        <h1 class="-my-28 mb-4 text-2xl font-bold tracking-tight leading-none text-white md:text-4xl lg:text-5xl">Перевозки грузов по необъятной<br>Омской области</h1>
+                        <br>
+                        <!-- содержание -->
+                        <p class="bg-black bg-opacity-50 rounded-[15px] text-lg font-normal text-white lg:text-xl  lg:px-48">ГрузоМагистр - надежный партнер для вашего груза.Мы предоставляем удобный спектр логистических и транспортных услуг, гарантируя, что ваши товары прибудут в целости и сохранности в срок.</p>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <!-- новости -->
+    <div>
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="py-10">
+                <h2 class="p-5 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Наши новости</h2>
+                <div class="mt-10 mb-4 max-w-screen-xl lg:grid lg:grid-cols-3 gap-4">
+                    
+                    <!-- новость 1 -->
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="p-2 rounded-t-lg" src="https://images.unsplash.com/photo-1497366811353-6870744d04b2?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                        </a>
+
+                        <!-- заголовок и содержание -->
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Новый офис</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Содержание.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#FF0015] rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-[#FF0015] dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                Интересно
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
                         </div>
-                        <div class="grid grid-cols-2 gap-4 mt-8">
-                            <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1">
-                            <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2">
+                    </div>
+
+                    <!-- новость 2 -->
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="p-2 rounded-t-lg" src="https://images.unsplash.com/photo-1601467995997-ac1ae9a8fff4?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                        </a>
+
+                        <!-- заголовок и содержание -->
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Эффективные машины</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Содержание.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#FF0015] rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-[#FF0015] dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                Интересно
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
                         </div>
-                        <button type="button" class="flex my-5 text-white dark:text-[#232323] bg-[#FF0015] hover:bg-[#FF5267] focus:ring-4 focus:outline-none font-medium rounded-lg text-xl sm:w-auto px-24 py-2.5 text-center"><a href="{{ route('about') }}">Узнайте больше о нашей компании</a></button>
+                    </div>
+
+                    <!-- новость 3 -->
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="p-2 rounded-t-lg" src="https://media.istockphoto.com/id/171582939/photo/truck-driver-on-german-autobahn.webp?b=1&s=170667a&w=0&k=20&c=SZR-QANOdwxQSs_dkkNbYyWiDDcDzBekzRY_NZQAlSg=" alt="" />
+                        </a>
+
+                        <!-- заголовок и содержание -->
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Опытные рабочие</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Содержание.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#FF0015] rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-[#FF0015] dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                Интересно
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- отзывы -->
+            <div>
+                <h2 class="p-5 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Наши отзывы</h2>
+                <div class="mt-10 mb-4 max-w-screen-xl lg:grid lg:grid-cols-3 gap-4">
+
+                    <!-- отзыв 1 -->
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="p-5">
+                            <p class="mb-3 font-medium text-gray-900 dark:text-gray-400">Содержание отзыва.</p>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Имя Фамилия</p>
+                        </div>
+                    </div>
+
+                    <!-- отзыв 2 -->
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="p-5">
+                            <p class="mb-3 font-medium text-gray-900 dark:text-gray-400">Содержание отзыва.</p>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Имя Фамилия</p>
+                        </div>
                     </div>
                     
-                </section>
+                    <!-- отзыв 3 -->
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div class="p-5">
+                            <p class="mb-3 font-medium text-gray-900 dark:text-gray-400">Содержание отзыва.</p>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Имя Фамилия</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
