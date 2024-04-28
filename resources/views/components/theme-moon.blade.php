706f9f5844fc9aab234d6fc7276f6e0beb7421e0
Eldar Zhamilev <waxenov@mail.ru>
2024-04-28 23:21:14 +0600

    Созданы обозначения кнопки переключения темы

---
+++ resources/views/components/theme-moon.blade.php
@@ -0,0 +1,14 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 169.4 169.4" {{ $attributes }}>
+    <defs>
+        <style>.moon-1{fill:#dcdcdc;}.moon-2{fill:#909090;}.moon-3{fill:#dbd7d2;}</style>
+    </defs>
+    <rect class="moon-1" x="24.2" y="24.2" width="121" height="121"/>
+    <rect class="moon-2" y="96.8" width="48.4" height="24.2"/>
+    <rect class="moon-3" x="120.9" y="48.5" width="48.5" height="24.2"/>
+    <rect class="moon-3" x="96.8" y="24.3" width="48.3" height="24.2"/>
+    <rect class="moon-2" x="24.2" y="121" width="48.4" height="24.2"/>
+    <rect class="moon-3" x="48.4" width="72.6" height="24.2"/>
+    <rect class="moon-2" x="48.35" y="145.2" width="72.7" height="24.2"/>
+    <rect class="moon-2" y="48.4" width="24.2" height="72.6"/>
+    <rect class="moon-3" x="145.2" y="48.4" width="24.2" height="72.6"/>
+</svg>
---
+++ resources/views/components/theme-sun.blade.php
@@ -0,0 +1,14 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 169.4 169.4" {{ $attributes }}>
+    <defs>
+        <style>.sun-1{fill:#ff9218;}.sun-2{fill:#faeedd;}</style>
+    </defs>
+    <rect class="sun-1" x="24.25" y="24.25" width="120.9" height="120.9"/>
+    <rect class="sun-2" x="48.4" width="72.6" height="24.2"/>
+    <rect class="sun-2" x="48.35" y="145.2" width="72.7" height="24.2"/>
+    <rect class="sun-2" y="48.4" width="24.2" height="72.6"/>
+    <rect class="sun-2" x="145.2" y="48.4" width="24.2" height="72.6"/>
+    <rect class="sun-2" x="24.15" y="24.2" width="24.2" height="24.2"/>
+    <rect class="sun-2" x="121" y="24.2" width="24.2" height="24.2"/>
+    <rect class="sun-2" x="24.15" y="121" width="24.2" height="24.2"/>
+    <rect class="sun-2" x="121" y="121" width="24.2" height="24.2"/>
+</svg>
