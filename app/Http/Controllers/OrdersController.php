ecd37353c960a2a84e90851e616dd16ad89d93cf
Eldar Zhamilev <waxenov@mail.ru>
2024-04-28 22:46:36 +0600

    Добавлена страница деталей с принятыми заказами и взаимодействие со статусом заказа для перевозчика

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\formloadmodel;
+use Illuminate\Support\Facades\Auth;
 
 class OrdersController extends Controller
 {
@@ -11,6 +12,7 @@ class OrdersController extends Controller
     //для заказчика перевозчика и админа
     public function showOrders(Request $request)
     {   
+        
         //вывод всех пользователей и заказов
         $user = $request->user();
         $formData = formloadmodel::all();
@@ -42,9 +44,11 @@ class OrdersController extends Controller
             $userOrders = $userOrders->get();
             
             return view('client.orders', compact('userOrders'));
+
             
         } elseif ($user->hasRole('carrier')) {
             
+            $formData = formloadmodel::where('status', 'pending')->get();
 
         //поиск из введенных данных
         $searchQuery = $request->input('search');
@@ -52,7 +56,7 @@ class OrdersController extends Controller
         //Перевозчик
         if (empty($searchQuery)) {
             //запрос пустой - вывод всех заказов
-            $orders = formloadmodel::all();
+            $orders = formloadmodel::where('status', '!=', 'confirmed');
         } else {
 
             $formData = formloadmodel::where('surname', 'like', "%$searchQuery%")
@@ -70,6 +74,7 @@ class OrdersController extends Controller
             ->orWhere('id', 'like', "%$searchQuery%")
             ->get();
         }
+
         //все заказы на странице перевозчику
         return view('carrier.orders', compact('formData', 'searchQuery'));
 
@@ -103,11 +108,98 @@ class OrdersController extends Controller
         //все заказы на странице админу
         return view('carrier.orders', compact('formData', 'searchQuery'));
 
-        } else {
-            abort(403, 'Доступ запрещен');
         }
     }
-    
+
+    //детали заказа
+    public function orderDetails(Request $request)
+    {
+                //вывод всех пользователей и заказов
+                $user = $request->user();
+                $formData = formloadmodel::all();
+
+                //Заказчик
+                if ($user->hasRole('client')) {
+            
+                    //поиск из введенных данных
+                    $searchQuery = $request->input('search');
+
+                    //вывод заказов для заказчика
+                    $userOrders = formloadmodel::where('phone', $user->phone);
+
+                     //запрос пустой - вывод всех заказов заказчика
+                    if (!empty($searchQuery)) {
+                        $userOrders->where(function($query) use ($searchQuery) {
+                            $query->where('cargo_type', 'like', "%$searchQuery%")
+                            ->orWhere('cargo_describe', 'like', "%$searchQuery%")
+                            ->orWhere('load_place', 'like', "%$searchQuery%")
+                            ->orWhere('unload_place', 'like', "%$searchQuery%")
+                            ->orWhere('ready_date', 'like', "%$searchQuery%")
+                            ->orWhere('weight', 'like', "%$searchQuery%")
+                            ->orWhere('created_at', 'like', "%$searchQuery%")
+                            ->orWhere('truck_type', 'like', "%$searchQuery%")
+                            ->orWhere('id', 'like', "%$searchQuery%");
+                        });
+                    }
+                     //отфильтрованные заказы на странице заказчику
+                    $userOrders = $userOrders->get();
+                    
+                    return view('details', compact('userOrders'));
+                }
+    }
+
+    //принятие заказа
+    public function confirmed($id)
+    {
+        $order = formloadmodel::findOrFail($id);
+        if ($order->status === 'pending') {
+            $order->status = 'confirmed';
+            $order->confirmed_at = now();
+            $order->id_carrier = Auth::id(); // номер перевозчика
+            $order->save();
+        }
+        return redirect()->back();
+    }
+
+    public function traffic($id)
+    {
+        $order = formloadmodel::findOrFail($id);
+        if ($order->status === 'confirmed') {
+            $order->status = 'traffic';
+            $order->traffic_at = now();
+            $order->save();
+        }
+        return redirect()->back();
+    }
+
+    public function delivered($id)
+    {
+        $order = formloadmodel::findOrFail($id);
+        if ($order->status === 'traffic') {
+            $order->status = 'delivered';
+            $order->delivered_at = now();
+            $order->save();
+        }
+        return redirect()->back();
+    }
+
+    //вывод принятых заказов
+    public function showStatusOrders(Request $request)
+    {
+        $user = $request->user();
+        if (!$user->hasRole('carrier')) {
+            abort(403, 'Доступ запрещен');
+        }
+
+        $statusOrders = formloadmodel::where('id_carrier', $user->id)
+                                        ->whereNotIn('status', ['pending']) // Исключаем заказы со статусом "pending"
+                                        ->get();
+
+        return view('carorders', compact('statusOrders'));
+    }
+
+
+
     //удаление заказа по id и обновление страницы
     public function destroy($id)
     {   
